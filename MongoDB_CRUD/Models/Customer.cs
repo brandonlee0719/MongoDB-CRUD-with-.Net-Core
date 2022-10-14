9c31fbdcce034091fcb10233fef6187c47f464b6
bestmycode <bestmycode610@gmail.com>
2022-10-14 14:22:20 +0900

    swagger version 3.0

---
+++ MongoDB_CRUD/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDB_CRUD.Models
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("firstName")]
+        public string FirstName { get; set; }
+
+        [BsonElement("lastName")]
+        public string LastName { get; set; }
+
+        [BsonElement("contact")]
+        public string Contact { get; set; }
+        [BsonElement("country")]
+        public string Country { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; }
+    }
+}
---
+++ MongoDB_CRUD/Startup.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using MongoDB_CRUD.Models;
+using MongoDB_CRUD.Repository;
+using MongoDB_CRUD.Services;
+
+namespace MongoDB_CRUD
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v3", new OpenApiInfo { Title = "MongoDB CRUD API BY DAVID", Version = "v3" });
+            });
+
+            services.Configure<DbConfiguration>(Configuration.GetSection("MongoDbConnection"));
+            services.AddScoped<ICustomerService,CustomerService>();
+            services.AddScoped<ICustomerRepository,CustomerRepository>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v3/swagger.json", "MongoDB CURD BY DAVID");
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
